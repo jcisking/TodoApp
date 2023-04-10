2d0c3d027cded157ee954ac2979192890721eb4e
jcisking <krisloeffler@gmail.com>
2023-04-09 17:47:41 -0700

    more building

--- app/src/main/java/com/example/todoapp/ui/TodoApp.kt
+++ app/src/main/java/com/example/todoapp/ui/TodoApp.kt
@@ -1,19 +1,38 @@
 package com.example.todoapp.ui
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.todoapp.R
 
 @Composable
 fun TodoApp() {
-    Column() {
-        Image(painterResource(id = R.drawable.bg_desktop_dark), contentDescription = null)
-        Text(text = "dsfasfdk")
-    }
-    
 
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+
+            painter = painterResource(id = R.drawable.bg_mobile_dark),
+            contentDescription = null
+        )
+        Column {
+           TodoTopBar()
+
+        }
+
+    }
+}
+
+@Composable
+fun TodoTopBar() {
+    Row() {
+        Text(text = stringResource(id = R.string.top_bar_text) )
+        Image(painter = painterResource(id = R.drawable.icon) , contentDescription = null)
+    }
 }

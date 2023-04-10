29643b134eede9d5f65633e963ea06c29fa89a2e
jcisking <krisloeffler@gmail.com>
2023-04-09 18:50:14 -0700

    more building

--- app/src/main/java/com/example/todoapp/ui/TodoApp.kt
+++ app/src/main/java/com/example/todoapp/ui/TodoApp.kt
@@ -1,12 +1,15 @@
 package com.example.todoapp.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -14,25 +17,34 @@ import com.example.todoapp.R
 
 @Composable
 fun TodoApp() {
-
     Box(modifier = Modifier.fillMaxSize()) {
         Image(
-
+            modifier = Modifier.fillMaxWidth().height(200.dp),
+            contentScale = ContentScale.Crop,
             painter = painterResource(id = R.drawable.bg_mobile_dark),
             contentDescription = null
         )
-        Column {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp),
+//            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
            TodoTopBar()
 
         }
-
     }
 }
 
 @Composable
-fun TodoTopBar() {
-    Row() {
+fun TodoTopBar(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .background(Color.Blue)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
         Text(text = stringResource(id = R.string.top_bar_text) )
-        Image(painter = painterResource(id = R.drawable.icon) , contentDescription = null)
+        Icon(painter = painterResource(id = R.drawable.sun) , contentDescription = null)
     }
 }

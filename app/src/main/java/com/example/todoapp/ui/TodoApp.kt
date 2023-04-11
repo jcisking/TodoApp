350205ec738316d02889182a129e6c45a4a15366
jcisking <krisloeffler@gmail.com>
2023-04-10 23:18:11 -0700

    another commit

--- app/src/main/java/com/example/todoapp/ui/TodoApp.kt
+++ app/src/main/java/com/example/todoapp/ui/TodoApp.kt
@@ -3,9 +3,7 @@ package com.example.todoapp.ui
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -24,7 +22,9 @@ fun TodoApp() {
             .background(MaterialTheme.colors.background)
     ) {
         Image(
-            modifier = Modifier.fillMaxWidth().height(200.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp),
             contentScale = ContentScale.Crop,
             painter = painterResource(id = R.drawable.bg_mobile_dark),
             contentDescription = null
@@ -35,7 +35,9 @@ fun TodoApp() {
                 .padding(start = 20.dp, end = 20.dp, top = 40.dp),
 //            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-           TodoTopBar()
+            TodoTopBar()
+            TodoListItem(modifier = Modifier.padding(top = 30.dp))
+
 
         }
     }
@@ -60,6 +62,29 @@ fun TodoTopBar(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun TodoListItem() {
+fun TodoListItem(
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        IconButton(onClick = { /*TODO*/ }, modifier = Modifier) {
+            Icon(painter = painterResource(id = R.drawable.icon_cross), contentDescription = null)
+            Icon(
+                modifier = Modifier.background(Color.Blue),
+                painter = painterResource(id = R.drawable.radio_button_unchecked_fill0_wght400_grad0_opsz48),
+                tint = Color.Green,
+                contentDescription = null
+            )
+        }
+        Text(text = "adklsdf")
+
+        Icon(
+            painter = painterResource(id = R.drawable.icon_cross),
+            contentDescription = null
+        )
+
+        
+    }
 
 }

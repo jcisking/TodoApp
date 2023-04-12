d0c5aac55fdf4b120d15ec328e1daf822c0b94af
jcisking <krisloeffler@gmail.com>
2023-04-11 23:07:39 -0700

    more building

---
+++ app/src/main/java/com/example/todoapp/data/local/TodoRepo.kt
@@ -0,0 +1,4 @@
+package com.example.todoapp.data.local
+
+object TodoRepo {
+}
---
+++ app/src/main/java/com/example/todoapp/ui/TodoViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.todoapp.ui
+
+import androidx.lifecycle.ViewModel
+
+class TodoViewModel: ViewModel() {
+
+
+
+
+    fun addNewTodo() {
+
+    }
+}

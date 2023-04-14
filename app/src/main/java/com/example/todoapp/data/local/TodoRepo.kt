df4ae5b1ea02d6e5f80175d93d7ce08fdc30d6ed
jcisking <krisloeffler@gmail.com>
2023-04-13 20:27:04 -0700

    modified view model

--- app/src/main/java/com/example/todoapp/data/local/TodoRepo.kt
+++ app/src/main/java/com/example/todoapp/data/local/TodoRepo.kt
@@ -3,6 +3,12 @@ package com.example.todoapp.data.local
 import com.example.todoapp.data.Todo
 
 object TodoRepo {
-    val todoList: MutableList<Todo> = mutableListOf()
+    private val todoList: MutableList<Todo> = mutableListOf()
+    fun addNewTodo(item: Todo) {
+        todoList.add(item)
+    }
+    fun getTodoList(): List<Todo> {
+        return todoList.toList()
+    }
 }
 
--- app/src/main/java/com/example/todoapp/ui/TodoViewModel.kt
+++ app/src/main/java/com/example/todoapp/ui/TodoViewModel.kt
@@ -5,15 +5,22 @@ import com.example.todoapp.data.Todo
 import com.example.todoapp.data.local.TodoRepo
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class TodoViewModel: ViewModel() {
-    private val _uiState = MutableStateFlow(TodoUiState(listOfTodos = TodoRepo.todoList))
+    private val _uiState = MutableStateFlow(TodoUiState(listOfTodos = TodoRepo.getTodoList()))
     val uiState = _uiState.asStateFlow()
 
 
 
 
     fun addNewTodo(newTodoItem: Todo) {
-        TodoRepo.todoList.add(newTodoItem)
+        TodoRepo.addNewTodo(newTodoItem)
+        val updatedTodoList = TodoRepo.getTodoList()
+        _uiState.update {
+            it.copy(
+                listOfTodos = updatedTodoList
+            )
+        }
     }
 }

21766cc9fee35ff27a372aaf5c47a5bc767dbbcb
jcisking <krisloeffler@gmail.com>
2023-04-15 14:51:34 -0700

    added BOM and updated other dependencies. Still getting duplicate classes error

--- app/src/main/java/com/example/todoapp/ui/TodoApp.kt
+++ app/src/main/java/com/example/todoapp/ui/TodoApp.kt
@@ -10,8 +10,6 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
